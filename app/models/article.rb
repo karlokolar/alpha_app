7a440ad2bff4b0a415755548b1fce659405835d9
Karlo Kolar <alien@kolar.link>
2016-12-17 11:14:57 +0100

    Create new action and all

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-	validate :title, presence: true, length: {minimum: 3, maximum: 24 }
-	validate :description, presence: true, length: {minimum: 12, maximum: 96 }
+	validates :title, presence: true, length: {minimum: 3, maximum: 24 }
+	validates :description, presence: true, length: {minimum: 12, maximum: 96 }
 
 end
